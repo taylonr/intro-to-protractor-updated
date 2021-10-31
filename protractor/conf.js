0fd7ed40f83622400621605e99241b6cd9f6b1ca
Nate Tyalor <taylonr@gmail.com>
2021-10-31 16:31:35 -0500

    Setting up first test

--- protractor/conf.js
+++ protractor/conf.js
@@ -1,15 +1,9 @@
 exports.config = {
-  seleniumAddress: 'http://localhost:4444/wd/hub',
-  specs: ['./**/*.spec.js'],
+  seleniumAddress: "http://localhost:4444/wd/hub",
+  specs: ["./ratings/event.list.item.spec.js"],
 
-  suites: {
-    smoke: './smoke/*.spec.js',
-    longRunning: './ratings/a.bunch.of.long.tests.spec.js',
-    full: './**/*.spec.js'
-  },
-
-  onPrepare: function(){
-    browser.driver.manage().window().setPosition(0,0);
+  onPrepare: function () {
+    browser.driver.manage().window().setPosition(0, 0);
     browser.driver.manage().window().setSize(1280, 720);
-  }
-}
+  },
+};
--- protractor/ratings/really.long.test.spec.js
+++
@@ -1,6 +0,0 @@
-describe("A really long test", function () {
-  it("Should take a long time", function () {
-    browser.get("http://localhost:3000/#!/EventRatings");
-    browser.driver.sleep(10000);
-  });
-});
