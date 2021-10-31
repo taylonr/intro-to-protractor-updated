efbb310611a42f265b0e0453b8e88ab6043a65b2
Nate Tyalor <taylonr@gmail.com>
2021-10-31 16:43:56 -0500

    Making all tests pass

--- protractor/conf.js
+++ protractor/conf.js
@@ -1,9 +1,15 @@
 exports.config = {
-  seleniumAddress: "http://localhost:4444/wd/hub",
-  specs: ["./ratings/event.list.item.spec.js"],
+  seleniumAddress: 'http://localhost:4444/wd/hub',
+  specs: ['./**/*.spec.js'],
 
-  onPrepare: function () {
-    browser.driver.manage().window().setPosition(0, 0);
-    browser.driver.manage().window().setSize(1280, 720);
+  suites: {
+    smoke: './smoke/*.spec.js',
+    longRunning: './ratings/a.bunch.of.long.tests.spec.js',
+    full: './**/*.spec.js'
   },
-};
+
+  onPrepare: function(){
+    browser.driver.manage().window().setPosition(0,0);
+    browser.driver.manage().window().setSize(1280, 720);
+  }
+}
