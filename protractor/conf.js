e17ec440dcbe543ff62cab2c1323784fa0f06624
Nate Tyalor <taylonr@gmail.com>
2021-10-31 18:07:50 -0500

    Main state

--- protractor/conf.js
+++ protractor/conf.js
@@ -1,6 +1,6 @@
 exports.config = {
   seleniumAddress: "http://localhost:4444/wd/hub",
-  specs: ["./ratings/event.list.item.spec.js"],
+  specs: [],
 
   onPrepare: function () {
     browser.driver.manage().window().setPosition(0, 0);
